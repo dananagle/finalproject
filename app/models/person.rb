57a33ae7908b1940685478271393f3eeed760e1d
dananagle <dananagle@Danas-Air.attlocal.net>
2015-07-19 15:34:55 -0500

    added validations

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,2 +1,12 @@
 class Person < ActiveRecord::Base
+  validates_presence_of :name
+  validates_presence_of :website
+  validates_presence_of :address  
+  validates_presence_of :user_id
+  validates_presence_of :eyecolor
+  validates_presence_of :haircolor
+  validates_presence_of :height
+  validates_presence_of :bust
+  validates_presence_of :hips
+  belongs_to :user
 end
