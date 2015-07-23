8856964f2850f21eef93d1b4218829391c8dfb00
dananagle <dananagle@Danas-MacBook-Air.local>
2015-07-23 17:39:25 -0500

    fixed columns removed search

--- config/application.rb
+++ config/application.rb
@@ -27,7 +27,8 @@ module Projectdiscern
         s3_credentials: {
             bucket: ENV['AWS_BUCKET'],
             access_key_id: ENV['AWS_ACCESS_KEY_ID'],
-            secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
+            secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+            url: ':s3_domain_url'
         }
     }
   end
